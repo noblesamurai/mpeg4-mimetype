e8d3ce45d721f5b25c0155da6257f6611fc10b88
Tim Allen <tim@noblesamurai.com>
2018-09-18 15:54:10 +1000

    Chunking version.

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const fs = require('fs');
 module.exports = function (filePath) {
   return new Promise((resolve, reject) => {
     const mp4boxfile = new MP4Box.createFile(); /* eslint-disable-line new-cap */
+    const stream = fs.createReadStream(filePath);
+
     mp4boxfile.onReady = (info) => {
-      console.log(info.mime);
+      stream.destroy();
       const codecs = [];
-      console.log('is fragmented: ' + info.isFragmented);
       for (var t = 0; t < info.tracks.length; ++t) {
-        console.log('track #' + t + ' codec string: ' + info.tracks[t].codec);
         codecs.push(info.tracks[t].codec);
       }
       const codecStr = 'video/mp4; codecs="' + codecs.join(', ') + '"';
-      console.log(codecStr);
       resolve(codecStr);
     };
 
-    var arrayBuffer = new Uint8Array(fs.readFileSync(filePath)).buffer;
-    arrayBuffer.fileStart = 0;
-    mp4boxfile.appendBuffer(arrayBuffer);
+    mp4boxfile.onError = (err) => {
+      stream.destroy();
+      return reject(err);
+    };
+
+    let offset = 0;
+    stream.on('data', function (chunk) {
+      var arrayBuffer = new Uint8Array(chunk).buffer;
+      arrayBuffer.fileStart = offset;
+      offset += chunk.byteLength;
+      mp4boxfile.appendBuffer(arrayBuffer);
+    });
+    stream.on('error', reject);
   });
 };
