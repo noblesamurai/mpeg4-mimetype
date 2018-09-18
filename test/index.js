e8e9eedaec03b496d2e50f7ae0646cf74de3bfc0
Tim Allen <tim@noblesamurai.com>
2018-09-19 08:32:52 +1000

    Fix test description.

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const expect = require('chai').expect;
 
 describe('index', function () {
-  it('gives a mine type string', async function () {
+  it('gives a mime type string', async function () {
     const mpeg4MimeType = require('..');
     const details = await mpeg4MimeType(path.join(__dirname, 'fixtures', 'SampleVideo.mp4'));
     expect(details).to.be.a('string');
