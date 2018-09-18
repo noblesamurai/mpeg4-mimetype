0d4e419810c153c0d356bbe28463d77c0c366678
Tim Allen <tim@noblesamurai.com>
2018-09-18 15:42:41 +1000

    First cut at non-chunked version.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const MP4Box = require('mp4box');
-const mine = new MP4Box.createFile(); /* eslint-disable-line new-cap */
-console.log(mine);
+const fs = require('fs');
+
+module.exports = function (filePath) {
+  return new Promise((resolve, reject) => {
+    const mp4boxfile = new MP4Box.createFile(); /* eslint-disable-line new-cap */
+    mp4boxfile.onReady = (info) => {
+      console.log(info.mime);
+      const codecs = [];
+      console.log('is fragmented: ' + info.isFragmented);
+      for (var t = 0; t < info.tracks.length; ++t) {
+        console.log('track #' + t + ' codec string: ' + info.tracks[t].codec);
+        codecs.push(info.tracks[t].codec);
+      }
+      const codecStr = 'video/mp4; codecs="' + codecs.join(', ') + '"';
+      console.log(codecStr);
+      resolve(codecStr);
+    };
+
+    var arrayBuffer = new Uint8Array(fs.readFileSync(filePath)).buffer;
+    arrayBuffer.fileStart = 0;
+    mp4boxfile.appendBuffer(arrayBuffer);
+  });
+};
--- test/index.js
+++ test/index.js
@@ -1,5 +1,11 @@
+const path = require('path');
+const expect = require('chai').expect;
+
 describe('index', function () {
-  it('works', function () {
-    require('..');
+  it('gives a mine type string', async function () {
+    const mpeg4MimeType = require('..');
+    const details = await mpeg4MimeType(path.join(__dirname, 'fixtures', 'SampleVideo.mp4'));
+    expect(details).to.be.a('string');
+    expect(details).to.match(/video\/mp4; codecs="[a-z0-9.]+, [a-z0-9.]+"/);
   });
 });
