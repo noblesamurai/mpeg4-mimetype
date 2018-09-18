2dac57ea8f887df25045d93f2c782f9fbee0eed3
Tim Allen <tim@noblesamurai.com>
2018-09-19 08:55:52 +1000

    Test for alien file type.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const expect = require('chai').expect;
+const mpeg4MimeType = require('..');
 
 describe('index', function () {
   it('gives a mime type string', async function () {
-    const mpeg4MimeType = require('..');
     const details = await mpeg4MimeType(path.join(__dirname, 'fixtures', 'SampleVideo.mp4'));
     expect(details).to.be.a('string');
     expect(details).to.match(/video\/mp4; codecs="[a-z0-9.]+, [a-z0-9.]+"/);
+  });
+  it('rejects the promise if the file is not interpretable', async function () {
+    this.timeout(10000);
+    return mpeg4MimeType(path.join(__dirname, 'fixtures', '1289472.opus')).catch((err) => {
+      expect(err).to.be.ok();
+      return Promise.resolve('ok');
+    }).then((result) => {
+      expect(result).to.equal('ok');
+    });
   });
 });
