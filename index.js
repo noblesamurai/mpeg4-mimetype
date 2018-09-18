e16509f649ec8d54344d5f00138da454792fb6f9
Tim Allen <tim@noblesamurai.com>
2018-09-18 15:08:20 +1000

    Manage dependency on mp4box better.

---
+++ index.js
@@ -0,0 +1,3 @@
+const MP4Box = require('mp4box');
+const mine = new MP4Box.createFile(); /* eslint-disable-line new-cap */
+console.log(mine);
---
+++ test/index.js
@@ -0,0 +1,5 @@
+describe('index', function () {
+  it('works', function () {
+    require('..');
+  });
+});
